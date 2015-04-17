7a1b65786f19e7d4c13dd29357f4ec652cb2bfad
DanielAndreasen <daniel.andreasen@astro.up.pt>
2015-04-17 18:38:44 +0100

    Changed test files and code

--- test_models.py
+++ test_models.py
@@ -15,33 +15,42 @@ py = sys.argv[1]
 fo = sys.argv[2]
 
 
-d1 = np.genfromtxt(py, skip_header=3, skip_footer=7)
+d1 = np.loadtxt(py)
 d2 = np.genfromtxt(fo, skip_header=3, skip_footer=7)
+# d2 = np.genfromtxt(fo, skip_header=3, skip_footer=7)
 
 
-plt.plot(d1[:, 0], d1[:, 1], 'o')
-plt.plot(d2[:, 0], d2[:, 1], 'o')
-
-plt.figure()
-
-plt.plot(d1[:, 0], d1[:, 2], 'o')
-plt.plot(d2[:, 0], d2[:, 2], 'o')
+#plt.plot(d1[:, 0], 100*(d1[:, 1]-d2[:,1])/d1[:,1], 'o')
+#plt.hlines(np.median(100*(d1[:, 1]-d2[:,1])/d1[:,1]), d1[0, 0], d1[-1, 0], 'r')
+plt.plot(d2[:, 0], d2[:, 1], 'o',d1[:, 0], d1[:, 1], 'o')
+plt.title("Temperature")
 
 plt.figure()
-plt.plot(d1[:, 0], d1[:, 3], 'o')
-plt.plot(d2[:, 0], d2[:, 3], 'o')
+
+#plt.plot(d1[:, 0], 100*(d1[:, 2]-d2[:,2])/d1[:,2], 'o') 
+#plt.hlines(np.median(100*(d1[:, 2]-d2[:,2])/d1[:,2]), d1[0, 0], d1[-1, 0], 'r')
+plt.plot(d2[:, 0], d2[:, 2], 'o',d1[:, 0], d1[:, 2], 'o')
+plt.title("Presssure")
+
 
 plt.figure()
-plt.plot(d1[:, 0], d1[:, 4], 'o')
-plt.plot(d2[:, 0], d2[:, 4], 'o')
+#plt.plot(d1[:, 0], 100*(d1[:, 3]-d2[:,3])/d1[:,3], 'o')
+#plt.hlines(np.median(100*(d1[:, 3]-d2[:,3])/d1[:,3]), d1[0, 0], d1[-1, 0], 'r')
+plt.plot(d2[:, 0], d2[:, 3], 'o',d1[:, 0], d1[:, 3], 'o')
+plt.title("Numb of Electrons")
 
 plt.figure()
-plt.plot(d1[:, 0], d1[:, 5], 'o')
-plt.plot(d2[:, 0], d2[:, 5], 'o')
+#plt.plot(d1[:, 0], 100*(d1[:, 4]-d2[:,4])/d1[:,4], 'o')
+#plt.hlines(np.median(100*(d1[:, 4]-d2[:,4])/d1[:,4]), d1[0, 0], d1[-1, 0], 'r')
+plt.plot(d2[:, 0], d2[:, 4], 'o',d1[:, 0], d1[:, 4], 'o')
+plt.title("Rossland absorption")
 
 plt.figure()
-plt.plot(d1[:, 0], d1[:, 6], 'o')
-plt.plot(d2[:, 0], d2[:, 6], 'o')
+#plt.plot(d1[:, 0], 100*(d1[:, 5]-d2[:,5])/d1[:,5], 'o')
+#plt.hlines(np.median(100*(d1[:, 5]-d2[:,5])/d1[:,5]), d1[0, 0], d1[-1, 0], 'r')
+
+plt.plot(d2[:, 0], d2[:, 5], 'o',d1[:, 0], d1[:, 5], 'o')
+plt.title("Accrad?")
 
 plt.show()
 
