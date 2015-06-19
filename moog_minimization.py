4c6f83c28c9a13a369e97e715117dee875ad5dd3
DanielAndreasen <daniel.andreasen@astro.up.pt>
2015-06-19 20:01:21 +0100

    Print the converging parameters to a file (minimization_profile.dat, and put upper bound on run per sub-loop

--- moog_minimization.py
+++ moog_minimization.py
@@ -6,6 +6,7 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 from runmoog import fun_moog_fortran, fun_moog
+import os
 
 
 def print_format(x):
@@ -14,7 +15,7 @@ def print_format(x):
 
 def minimize(x0, func, bounds=None,
              fix_teff=False, fix_logg=False, fix_feh=False, fix_vt=False,
-             eps=1e-7, iteration=100):
+             eps=1e-7, iteration=25):
     """
     Some doc
     """
@@ -29,10 +30,12 @@ def minimize(x0, func, bounds=None,
     res, slopeEP, slopeRW, abundances = func(x0)
     Abdiff = np.diff(abundances)[0]
     parameters = list(x0)
+    os.remove('minimization_profile.dat')
 
     N = 0
     while (res > eps) and (N < iteration):
-        while (abs(slopeEP) > 0.001) and not fix_teff:
+        N1 = 0
+        while (abs(slopeEP) > 0.001) and not fix_teff and N1 < 15:
             # For Teff
             s = np.sign(slopeEP)
             step_i = s * step[0]/abs(np.log(abs(slopeEP)+0.0005))**3
@@ -41,9 +44,12 @@ def minimize(x0, func, bounds=None,
             print_format(parameters)
             res, slopeEP, slopeRW, abundances = func(parameters)
             Abdiff = np.diff(abundances)[0]
+            N1 += 1
+            os.system('echo %s >> minimization_profile.dat' % ','.join(map(str, parameters)))
         N += 1
 
-        while (abs(Abdiff) > 0.001) and not fix_logg:
+        N2 = 0
+        while (abs(Abdiff) > 0.001) and not fix_logg and N2 < 15:
             # For logg
             s = -np.sign(Abdiff)
             step_i = s * step[1]/abs(np.log(abs(Abdiff)+0.0005))**3
@@ -52,17 +58,23 @@ def minimize(x0, func, bounds=None,
             print_format(parameters)
             res, slopeEP, slopeRW, abundances = func(parameters)
             Abdiff = np.diff(abundances)[0]
+            N2 += 1
+            os.system('echo %s >> minimization_profile.dat' % ','.join(map(str, parameters)))
         N += 1
 
         #       Input metal...   FeI abund.
-        while (parameters[2] != abundances[0]-7.47) and not fix_feh:
+        N3 = 0
+        while (parameters[2] != abundances[0]-7.47) and not fix_feh and N3 < 5:
             # For metalicity
             parameters[2] = abundances[0]-7.47
             res, slopeEP, slopeRW, abundances = func(parameters)
             print_format(parameters)
+            N3 += 1
+            os.system('echo %s >> minimization_profile.dat' % ','.join(map(str, parameters)))
         N += 1
 
-        while (abs(slopeRW) > 0.001) and not fix_vt:
+        N4 = 0
+        while (abs(slopeRW) > 0.001) and not fix_vt and N4 < 15:
             # For micro turbulence
             s = np.sign(slopeRW)
             step_i = s * step[2]/abs(np.log(abs(slopeRW)+0.0005))**3
@@ -72,11 +84,8 @@ def minimize(x0, func, bounds=None,
             print_format(parameters)
             res, slopeEP, slopeRW, abundances = func(parameters)
             Abdiff = np.diff(abundances)[0]
+            N4 += 1
+            os.system('echo %s >> minimization_profile.dat' % ','.join(map(str, parameters)))
         N += 1
 
     return parameters
-
-
-# x0 = (5777, 4.44, 0.0, 1.00)
-# minimize(x0, fun_moog_fortran)
-# minimize(x0, fun_moog)
