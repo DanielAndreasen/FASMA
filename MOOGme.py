987301cdabf6e1d8a6123443cc8c09c83ef4480c
DanielAndreasen <daniel.andreasen@astro.up.pt>
2015-07-07 00:25:31 +0100

    Adding some logging

--- MOOGme.py
+++ MOOGme.py
@@ -3,6 +3,7 @@
 
 # My imports
 from __future__ import division, print_function
+import logging
 import os
 import argparse
 import seaborn as sns
@@ -92,7 +93,7 @@ def _renaming(linelist, converged):
         cmd = 'cp summary.out %s.out' % linelist
     else:
         cmd = 'cp summary.out %s.NC.out' % linelist
-        os.system('cp minimization_profile.dat %s.profile.dat' % linelist)
+        #os.system('cp  minimization_profile.dat %s.profile.dat' % linelist)
 
     os.system(cmd)
 
@@ -116,6 +117,16 @@ def moogme(starLines, parfile='batch.par', model='kurucz95',
     # Daniel's late-night loggs. Maybe this should be changed. It is 02:12 AM saturday, and I'm tired...
     spectralType_g = {'I': 0, 'II': 1, 'III': 2, 'IV': 3, 'V': 4.5}
 
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.FileHandler('captain.log')
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
     # Setting the initial parameters
     if spt and not initial:
         Teff = spectralType_T[spt[0:2]]
@@ -133,10 +144,13 @@ def moogme(starLines, parfile='batch.par', model='kurucz95',
         os.system('cp %s batch.par' % parfile)
         rm_batch = True
 
+
     with open(starLines, 'r') as lines:
         for line in lines:
             if not line[0].isalpha():
+                logger.debug('Skipping header: %s' % line.strip())
                 continue
+            logger.info('Line list: %s' % line.strip())
             fix_teff = False
             fix_logg = False
             fix_feh = False
@@ -145,29 +159,43 @@ def moogme(starLines, parfile='batch.par', model='kurucz95',
             line = line.split(' ')
             if len(line) == 1:
                 initial = (5777, 4.44, 0.00, 1.00)
+                logger.info('Setting solar values {0}, {1}, {2}, {3}'.format(*initial))
                 # Update batch.par
                 _update_par(line_list=line[0])
             elif len(line) == 2:
+                logger.info('Spectral type given: %s' % line[1])
                 spt = line[1]
-                print(spt)
                 Teff = spectralType_T[spt[0:2]]
                 logg = spectralType_g[spt[2::]]
                 mic, _ = _getMic(Teff, logg)
                 initial = (Teff, logg, 0.00, mic)
+                logger.info('Initial parameters: {0}, {1}, {2}, {3}'.format(*initial))
                 _update_par(line_list=line[0])
             elif len(line) == 5:
+                logger.info('Initial parameters given by the user.')
                 initial = map(float, line[1::])
+                logger.info('Initial parameters: {0}, {1}, {2}, {3}'.format(*initial))
                 _update_par(line_list=line[0])
             elif len(line) == 6:
+                logger.info('Initial parameters given by user and some parameters fixed.')
                 initial = map(float, line[1:-1])
+                logger.info('Initial parameters: {0}, {1}, {2}, {3}'.format(*initial))
                 fix = line[-1].lower()
                 fix_teff = True if 'teff' in fix else False
                 fix_logg = True if 'logg' in fix else False
                 fix_feh  = True if 'feh'  in fix else False
                 fix_vt   = True if 'vt'   in fix else False
+                if fix_teff:
+                    logger.info('Effective temperature fixed at: %i' % initial[0])
+                elif fix_logg:
+                    logger.info('Surface gravity fixed at: %s' % initial[1])
+                elif fix_feh:
+                    logger.info('Metallicity fixed at: %s' % initial[2])
+                elif fix_vt:
+                    logger.info('Micro turbulence fixed at: %s' % initial[3])
                 _update_par(line_list=line[0])
             else:
-                print('You are a fool! Try again')
+                logger.error('Could not process information for this line list.')
                 continue
 
             # Setting the models to use
@@ -175,21 +203,25 @@ def moogme(starLines, parfile='batch.par', model='kurucz95',
                 raise NotImplementedError('Your request for type: %s is not available' % model)
 
             # Get the initial grid models
+            logger.info('Getting initial model grid')
             models, nt, nl, nf = _get_model(teff=initial[0], logg=initial[1], feh=initial[2])
+            logger.info('Initial interpolation of model...')
             inter_model = interpolator(models,
                                        teff=(initial[0], nt),
                                        logg=(initial[1], nl),
                                        feh=(initial[2], nf))
             save_model(inter_model, params=initial)
+            logger.info('Interpolation successful.')
 
+            logger.info('Starting the minization procedure...')
             parameters, converged = minimize(initial, fun_moog, bounds=model,
                                   fix_teff=fix_teff, fix_logg=fix_logg,
                                   fix_feh=fix_feh, fix_vt=fix_vt)
-
+            logger.info('Finished minization procedure')
             _renaming(line[0], converged)
 
-            print('\nCongratulation, you have won! Your final parameters are\n' + ', '.join(map(str,parameters)))
-            print(line[0])
+            #print('\nCongratulation, you have won! Your final parameters are\n' + ', '.join(map(str,parameters)))
+            #print(line[0])
     return parameters
 
 
