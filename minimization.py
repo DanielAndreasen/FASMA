ed107efbe6ae27c4bb5f664881c95d4aa33494d5
DanielAndreasen <daniel.andreasen@astro.up.pt>
2015-10-29 20:00:36 +0000

    Add second minimization routine. Problems with always 'bumping' the parameters.

--- minimization.py
+++ minimization.py
@@ -180,3 +180,105 @@ def minimize(x0, func, bounds="kurucz95", weights='null',
     converged = check_convergence(slopeRW, slopeEP, Abdiff, parameters[2], abundances[0])
 
     return parameters, converged
+
+
+def _bump(x):
+    """Bump to the values in the list, x"""
+    for i, xi in enumerate(x):
+        x[i] = np.random.normal(xi, abs(0.01*xi))
+    return x
+
+
+def minimize2(x0, func, bounds="kurucz95", weights='null',
+             fix_teff=False, fix_logg=False, fix_feh=False, fix_vt=False,
+             iteration=160, EPcrit=0.001, RWcrit=0.003, ABdiffcrit=0.01):
+    """
+    Sane minimization like a normal human being would do it.
+    """
+    global EPcriteria, RWcriteria, ABdiffcriteria
+    global f_teff, f_logg, f_vt, f_feh
+    EPcriteria, RWcriteria, ABdiffcriteria = EPcrit, RWcrit, ABdiffcrit
+    f_teff, f_logg, f_feh, f_vt = fix_teff, fix_logg, fix_feh, fix_vt
+    # Step size in Teff, logg, vt
+    step = (700, 1.50, 0.50)
+    if bounds.lower() == "kurucz95":
+        bounds = [3750, 39000, 0.0, 5.0, -3, 1, 0, 9.99]
+
+    res, slopeEP, slopeRW, abundances = func(x0)
+    Abdiff = np.diff(abundances)[0]
+    parameters = list(x0)
+    if check_convergence(slopeRW, slopeEP, Abdiff, parameters[2], abundances[0]):
+        return parameters, True
+    try:
+        os.remove('minimization_profile.dat')
+    except OSError:
+        pass
+    all_params = [parameters]
+
+    N = 0
+    while N < iteration:
+        # Step for Teff
+        if (abs(slopeEP) >= EPcriteria) and not fix_teff:
+            s = np.sign(slopeEP)
+            step_i = s * step[0]/abs(np.log(abs(slopeEP)+0.0005))**3
+            if abs(step_i) < 1:
+                step_i = s*1  # Minimum 1K
+            elif abs(step_i) > 17625:
+                step_i = s*17625  # Maximum 17625K (half the interval?)
+            parameters[0] += step_i
+            parameters[0] = check_bounds(parameters[0], bounds, 1)
+            parameters[0] = int(parameters[0])
+
+        # Step for VT
+        if (abs(slopeRW) >= RWcriteria) and not fix_vt:
+            s = np.sign(slopeRW)
+            step_i = s * step[2]/abs(np.log(abs(slopeRW)+0.0005))**3
+            if abs(step_i) < 0.01:
+                step_i = s*0.01  # Minimum vt step
+            elif abs(step_i) > 2.5:
+                step_i = s*2.5  # Maximum vt step
+            else:
+                step_i = step_i
+            parameters[3] += step_i
+            parameters[3] = 0.00 if parameters[3] < 0 else parameters[3]
+            parameters[3] = check_bounds(parameters[3], bounds, 7)
+            parameters[3] = round(parameters[3], 2)
+
+        # Step for logg
+        if (abs(Abdiff) >= ABdiffcrit) and not fix_logg:
+            s = -np.sign(Abdiff)
+            step_i = s * step[1]/abs(np.log(abs(Abdiff)+0.0005))**3
+            if abs(step_i) < 0.01:
+                step_i = s*0.01  # Minimum logg step
+            elif abs(step_i) > 2.5:
+                step_i = s*2.5  # Maximum logg step
+            parameters[1] += step_i
+            parameters[1] = check_bounds(parameters[1], bounds, 3)
+            parameters[1] = round(parameters[1], 2)
+
+        # Step for [Fe/H]
+        if not fix_feh:
+            parameters[2] = abundances[0]-7.47
+            parameters[2] = check_bounds(parameters[2], bounds, 5)
+            parameters[2] = round(parameters[2], 2)
+
+        if parameters in all_params:
+            parameters = _bump(parameters)
+            parameters[0] = int(parameters[0])
+            parameters[1] = round(parameters[1], 2)
+            parameters[2] = round(parameters[2], 2)
+            parameters[3] = round(parameters[3], 2)
+        all_params.append(parameters)
+        res, slopeEP, slopeRW, abundances = func(parameters, weights=weights)
+        Abdiff = np.diff(abundances)[0]
+        N += 1
+        print_format(parameters)
+        save_iteration(parameters)
+
+        if check_convergence(slopeRW, slopeEP, Abdiff, parameters[2], abundances[0]):
+            print 'Stopped in %i iterations' % N
+            return parameters, True
+
+    print 'Stopped in %i iterations' % N
+    c = check_convergence(slopeRW, slopeEP, Abdiff, parameters[2], abundances[0])
+    return parameters, c
