d3d700249e40fbbdf42b9afd55b10a8811fa27a4
MariaTsantaki <Maria.Tsantaki@astro.up.pt>
2016-08-02 17:07:34 -0500

    use ndim instead of rank

--- mpfit.py
+++ mpfit.py
@@ -2265,7 +2265,7 @@ class mpfit:
 
         if self.debug:
             print 'Entering calc_covar...'
-        if numpy.rank(rr) != 2:
+        if numpy.ndim(rr) != 2:
             print 'ERROR: r must be a two-dimensional matrix'
             return -1
         s = rr.shape
