62625112c1d0721e5926fd70bd9343f1102d71bf
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-04-21 11:31:21 +0100

    Only update if the line lists are the same, and add elements too

--- loggf_update.py
+++ loggf_update.py
@@ -36,18 +36,22 @@ def save_loggf(fname, df, linelist):
         df1 = pd.read_csv(fname, skiprows=2, delimiter=r'\s+',
                           names=cols)
         d = pd.merge(df1, df, left_on='wavelength', right_on='wavelength')
-        return d.loc[:, ['wavelength', 'X_x', 'EP_x', 'new_gf', 'EW']]
+        if len(d) != len(df):
+            return None
+        else:
+            return d.loc[:, ['wavelength', 'X_x', 'EP_x', 'new_gf', 'EW']]
 
     fname = 'rawLinelist/%s' % fname
     with open(linelist, 'r') as f:
         hdr = f.readline().strip()
 
     df = merge_loggf(fname, df)
-    fmt = ('%9.3f', '%10.1f', '%9.2f', '%9.3f', '%28.1f')
-    np.savetxt(linelist, df.values, fmt=fmt, header=hdr, comments='')
+    if df is not None:
+        fmt = ('%9.3f', '%10.1f', '%9.2f', '%9.3f', '%28.1f')
+        np.savetxt(linelist, df.values, fmt=fmt, header=hdr, comments='')
 
 
-def update_loggf(model, linelist, region='optical'):
+def update_loggf(model, linelist, region='EWoptical'):
     '''Use correct loggf dependent on the model atmosphere and star
 
     Inputs
@@ -57,7 +61,7 @@ def update_loggf(model, linelist, region='optical'):
     linelist : str
       Filename of the linelist to update
     region : str
-      Spectral region: "optical" or "NIR"
+      Spectral region: "EWoptical", "ABoptical", or "EWNIR"
     '''
 
     if model not in ['kurucz95', 'marcs']:
@@ -68,13 +72,19 @@ def update_loggf(model, linelist, region='optical'):
     df = pd.read_csv(linelist, skiprows=1, delimiter=r'\s+',
                      names=('wavelength', 'X', 'EP', 'loggf', 'EW'))
 
-    if region == 'optical':
+    if region == 'EWoptical':
         if model == 'kurucz95':
             loggf = 'Sousa2007_opt_kurucz.lst'
         elif model == 'marcs':
             loggf = 'Sousa2007_opt_marcs.lst'
 
-    elif region == 'NIR':
+    elif region == 'ABoptical':
+        if model == 'kurucz95':
+            loggf = 'Neves2009_opt_kurucz.lst'
+        elif model == 'marcs':
+            loggf = 'Neves2009_opt_marcs.lst'
+
+    elif region == 'EWNIR':
         if model == 'kurucz95':
             loggf = 'Andreasen2016_nir_kurucz.lst'
         elif model == 'marcs':
