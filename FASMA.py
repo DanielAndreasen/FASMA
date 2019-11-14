1d4042705c5180a6e609d08e7455d3d893e86392
MariaTsantaki <Maria.Tsantaki@astro.up.pt>
2019-11-14 23:44:15 +0100

    Revert "driver for element abund"
    This reverts commit b14939adf32332410b97307f1fa7de6c73b6818a.

--- FASMA.py
+++ FASMA.py
@@ -1,52 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
+
 from __future__ import print_function
-from synthDriver import synthMethod
+from ewDriver import EWmethod
+from abundanceDriver import AbundanceDriver
+from aresDriver import aresdriver
 from gooey import Gooey, GooeyParser
 
-def synth(args):
-    '''Driver for the synthesis method for stellar parameters.
-    '''
 
+def ew(args):
+    '''Driver for the EW method'''
     fout = ''
     linelist = args.linelist.rpartition('/')[-1]
-    inter_file = args.inter_file.rpartition('/')[-1]
-    if not args.temperature:
-        print('Temperature was set to 5777 K')
-        args.temperature = 5777
-    if not args.surfacegravity:
-        print('Surface gravity was set to 4.44 dex')
-        args.surfacegravity = 4.44
-    if not args.FeH:
-        args.FeH = 0.0
-    if not args.microturbulence:
-        print('Microturbulence was set to 1.00 km/s')
-        args.microturbulence = 1.00
-    if not args.macroturbulence:
-        print('Macroturbulence was set to 3.21 km/s')
-        args.macroturbulence = 3.21
-    if not args.vsini:
-        print('Rotation was set to 1.90 km/s')
-        args.microturbulence = 1.90
-    fout += '%s %s %s %s %s %s %s ' % (linelist, args.temperature, args.surfacegravity, args.FeH, args.microturbulence, args.macroturbulence, args.vsini)
+    args.outlier = False if args.outlier == 'False' else args.outlier
+    if args.spectralType:
+        if not args.temperature and not args.surfacegravity:
+            fout += '%s spt:%s,' % (linelist, args.spectralType)
+        else:
+            print('Temperature and/or surface gravity set. Will not use spectral type.')
+    else:
+        if not args.temperature:
+            print('Warning: Temperature was set to 5777K')
+            args.temperature = 5777
+        if not args.surfacegravity:
+            print('Warning: Surface gravity was set to 4.44')
+            args.surfacegravity = 4.44
+        if not args.FeH:
+            args.FeH = 0.0
+        if not args.microturbulence:
+            print('Warning: Microturbulence was set to 1.00')
+            args.microturbulence = 1.00
+        fout += '%s %s %s %s %s ' % (linelist, args.temperature, args.surfacegravity, args.FeH, args.microturbulence)
 
-    fout += 'model:%s,step_wave:%s,step_flux:%s,inter_file:%s,limb:%s,damping:%s' % (args.model, args.step_wave, args.step_flux, inter_file, args.limb, args.damping)
-    if args.observations:
-        fout += ',observations:%s' % args.observations
-    if args.resolution:
-        fout += ',resolution:%s' % args.resolution
-    if args.snr:
-        fout += ',snr:%s' % args.snr
+    fout += 'model:%s,iterations:%s,weights:%s,RWcrit:%s,EPcrit:%s,ABdiffcrit:%s,MOOGv:%s,sigma:%s' % (args.model, args.Iterations, args.weights, args.RWslope, args.EPslope, args.Fedifference, args.MOOGv, args.sigma)
+    if args.outlier:
+        fout += ',outlier:%s' % args.outlier
     if args.refine:
         fout += ',refine'
-    if args.plot:
-        fout += ',plot'
-    if args.plot_res:
-        fout += ',plot_res'
-    if args.save:
-        fout += ',save'
-    if args.minimize:
-        fout += ',minimize'
     if args.Fixteff:
         fout += ',teff'
     if args.Fixgravity:
@@ -55,62 +45,89 @@ def synth(args):
         fout += ',feh'
     if args.Fixmicroturbulence:
         fout += ',vt'
-    if args.Fixmacroturbulence:
-        fout += ',vmac'
-    if args.Fixvsini:
-        fout += ',vsini'
-    with open('StarMe_synth.cfg', 'w') as f:
+    if args.teffrange:
+        fout += ',teffrange'
+    if args.autofixvt:
+        fout += ',autofixvt'
+    if args.tmcalc:
+        fout += ',tmcalc'
+    with open('StarMe_ew.cfg', 'w') as f:
         f.writelines(fout)
-    driver = synthMethod(cfgfile='StarMe_synth.cfg', overwrite=None)
-    driver.synthdriver()
+    EWmethod(overwrite=args.overwrite)
+    driver = EWmethod(cfgfile='StarMe_ew.cfg', overwrite=None)
+    parameters = driver.ewdriver()
 
 def abund(args):
-    '''Driver for the synthesis method for element abundances.
-    '''
+    """Driver for abundances"""
 
     fout = ''
     linelist = args.linelist.rpartition('/')[-1]
-    inter_file = args.inter_file.rpartition('/')[-1]
+
     if not args.temperature:
-        print('Temperature was set to 5777 K')
+        print('Warning: Temperature was set to 5777K')
         args.temperature = 5777
     if not args.surfacegravity:
-        print('Surface gravity was set to 4.44 dex')
+        print('Warning: Surface gravity was set to 4.44')
         args.surfacegravity = 4.44
     if not args.FeH:
         args.FeH = 0.0
     if not args.microturbulence:
-        print('Microturbulence was set to 1.00 km/s')
+        print('Warning: Microturbulence was set to 1.00')
         args.microturbulence = 1.00
-    if not args.macroturbulence:
-        print('Macroturbulence was set to 3.21 km/s')
-        args.macroturbulence = 3.21
-    if not args.vsini:
-        print('Rotation was set to 1.90 km/s')
-        args.microturbulence = 1.90
-    fout += '%s %s %s %s %s %s %s ' % (linelist, args.temperature, args.surfacegravity, args.FeH, args.microturbulence, args.macroturbulence, args.vsini)
+    fout += '%s %s %s %s %s ' % (linelist, args.temperature, args.surfacegravity, args.FeH, args.microturbulence)
 
-    fout += 'model:%s,step_wave:%s,step_flux:%s,inter_file:%s,limb:%s,damping:%s' % (args.model, args.step_wave, args.step_flux, inter_file, args.limb, args.damping)
-    if args.element:
-        fout += ',element:%s' % args.element
-    if args.observations:
-        fout += ',observations:%s' % args.observations
-    if args.resolution:
-        fout += ',resolution:%s' % args.resolution
-    if args.snr:
-        fout += ',snr:%s' % args.snr
-    if args.plot:
-        fout += ',plot'
-    if args.plot_res:
-        fout += ',plot_res'
-    if args.save:
-        fout += ',save'
-    with open('StarMe_synth.cfg', 'w') as f:
+    fout += 'model:%s,MOOGv:%s\n' % (args.model, args.MOOGv)
+
+    with open('StarMe_abund.cfg', 'w') as f:
         f.writelines(fout)
-    driver = synthMethod(cfgfile='StarMe_synth.cfg', overwrite=None)
-    driver.synthdriver()
+    driver = AbundanceDriver(cfgfile='StarMe_abund.cfg')
+    _ = driver.abundancedriver()
+    driver.to_screen()
 
-@Gooey(program_name='FASMA - Spectral Synthesis with MOOG',
+def ares(args):
+    """Driver for ARES"""
+
+    def rejt_from_snr(snr):
+        """Calculate rejt from SNR"""
+        return 1.0-(1.0/snr)
+
+    #rvmask = False if args.RVmask == 0 else args.RVmask
+    out = args.spectrum + '.ares' if not args.output else args.output
+
+    # Make the StarMe_ares.cfg file from Gooey
+    fout = args.linelist.rpartition('/')[2]
+    fout += ' %s' % args.spectrum
+    fout += ' lambdai:%s,lambdaf:%s,smoothder:%s' % (args.lambdai, args.lambdaf, args.smoothder)
+    fout += ',space:%s,lineresol:%s' % (args.space, args.lineresol)
+    fout += ',miniline:%s,EWcut:%s' % (args.miniline, args.EWcut)
+
+    if args.rejt:
+        rejt = args.rejt
+        rejt = 0.999 if rejt > 0.999 else rejt
+        fout += ',rejt:%s' % rejt
+    if args.SNR:
+        rejt = rejt_from_snr(args.SNR)
+        rejt = 0.999 if rejt > 0.999 else rejt
+        fout += ',rejt:%s' % rejt
+
+    if args.output:
+        fout += ',output:%s' % args.output
+    if args.RVmask:
+        fout += ',rvmask:%s' % args.RVmask
+    if args.force:
+        fout += ',force'
+
+    with open('StarMe_ares.cfg', 'w') as f:
+        f.writelines(fout)
+
+    aresdriver()
+    if args.output:
+        print('Congratulations! The final line list are here: linelist/%s.moog' % args.output)
+    else:
+        linelist_out = 'linelist/%s.moog' % args.spectrum.rpartition('/')[2].rpartition('.')[0]
+        print('Congratulations! The final line list are here: %s' % linelist_out)
+
+@Gooey(program_name='FASMA - spectral analysis',
        default_size=(900, 1000),
        image_dir='./img')
 
@@ -119,57 +136,63 @@ def main():
     :returns: the args
     '''
     parser = GooeyParser(description='Set parameters')
+
     subparsers = parser.add_subparsers()
 
+    # Common to all
     parent_parser = GooeyParser(add_help=False)
-    parent_parser.add_argument('--temperature',     help='(in K)',    default=5777,   type=int,   metavar='Effective temperature')
-    parent_parser.add_argument('--surfacegravity',  help='(in dex)',  default=4.44,   type=float, metavar='Surface gravity')
-    parent_parser.add_argument('--FeH',             help='(in dex)',  default='0.00', type=float, metavar='Metallicity')
-    parent_parser.add_argument('--microturbulence', help='(in km/s)', default=1.00,   type=float, metavar='Microturbulence')
-    parent_parser.add_argument('--macroturbulence', help='(in km/s)', default=3.21,   type=float, metavar='Macroturbulence')
-    parent_parser.add_argument('--vsini',           help='(in km/s)', default=1.90,   type=float, metavar='Rotational Velocity')
-    parent_parser.add_argument('--model',           help='Grid of models', default='kurucz95', choices=['kurucz95', 'apogee_kurucz', 'marcs'], metavar='Model atmosphere')
+    parent_parser.add_argument('--temperature',     help='(in K)',    default=5777,   type=int,   metavar='Temperature')
+    parent_parser.add_argument('--surfacegravity',  help='(in dex)',  default=4.44,   type=float, metavar='logg')
+    parent_parser.add_argument('--FeH',             help='(in dex)',  default='0.00', type=float, metavar='[Fe/H]')
+    parent_parser.add_argument('--microturbulence', help='(in km/s)', default=1.0,    type=float, metavar='Microturbulence')
+    parent_parser.add_argument('--MOOGv',           default='2014', choices=['2013', '2014', '2016'], type=str, metavar='MOOG version')
+    parent_parser.add_argument('--model',           help='Grid of models', default='kurucz95', choices=['kurucz95', 'apogee_kurucz', 'kurucz08', 'marcs', 'PHOENIX'], metavar='Model atmosphere')
 
-    synth_parser = subparsers.add_parser('Parameters', parents=[parent_parser], help='Spectal Synthesis')
-    synth_parser.add_argument('linelist',             help='List with atomic data', widget='FileChooser', metavar='Line List')
-    synth_parser.add_argument('--inter_file',         help='List of wavelength intervals', default='intervals.lst', widget='FileChooser', metavar='Intervals File')
-    synth_parser.add_argument('--observations',       help='Input formats: fits, dat, spec', widget='FileChooser', metavar='Observed Spectrum')
-    synth_parser.add_argument('--damping',            help='Parameter for MOOG', default='1', choices=['0', '1', '2'], type=str, metavar='Damping')
-    synth_parser.add_argument('--step_wave',          help='for synthetic spectrum', default=0.01, type=float, metavar='Step Wavelength')
-    synth_parser.add_argument('--step_flux',          help='for synthetic spectrum', default=3.0, type=float, metavar='Step Flux')
-    synth_parser.add_argument('--minimize',           help='Start minimization process', action='store_true', metavar='Minimization')
-    synth_parser.add_argument('--refine',             help='Refine parameters',   action='store_true', metavar='Refine Parameters')
-    synth_parser.add_argument('--Iterations',         help='Maximum number', default=100, type=int)
-    synth_parser.add_argument('--Fixteff',            help='Fix Teff',   action='store_true', metavar='Fix Temperature')
-    synth_parser.add_argument('--Fixgravity',         help='Fix logg',   action='store_true', metavar='Fix Gravity')
-    synth_parser.add_argument('--FixFeH',             help='Fix [Fe/H]', action='store_true', metavar='Fix Metallicity')
-    synth_parser.add_argument('--Fixmicroturbulence', help='Fix vt',     action='store_true', metavar='Fix Microturbulence')
-    synth_parser.add_argument('--Fixmacroturbulence', help='Fix vmac',   action='store_true', metavar='Fix Macroturbulence')
-    synth_parser.add_argument('--Fixvsini',           help='Fix vsini',  action='store_true', metavar='Fix Rotational Velocity')
-    synth_parser.add_argument('--resolution',         help='of the spectrograph', default=None, type=int, metavar='Resolution')
-    synth_parser.add_argument('--snr',                help='ratio of spectrum', default=None, type=float, metavar='Signal-to-noise')
-    synth_parser.add_argument('--plot',               help='Plot output', action='store_true', metavar='Plot')
-    synth_parser.add_argument('--plot_res',           help='Add residuals to the plot', action='store_true', metavar='Plot Residuals')
-    synth_parser.add_argument('--limb',               help='coefficient for rotation', default=0.6, type=float, metavar='Limb Darkening')
-    synth_parser.add_argument('--save',               help='Save synthetic spectrum', action='store_true', metavar='Save Output')
-    synth_parser.set_defaults(driver=synth)
+    # For the EW method
+    ew_parser = subparsers.add_parser('ew', parents=[parent_parser], help='EW method')
+    ew_parser.add_argument('linelist',             help='Input linelist file', widget='FileChooser')
+    ew_parser.add_argument('--spectralType',       help='(optional)', default=False, metavar='Spectral type')
+    ew_parser.add_argument('--Fixteff',            help='Fix Teff',   action='store_true', metavar='Fix temperature')
+    ew_parser.add_argument('--Fixgravity',         help='Fix logg',   action='store_true', metavar='Fix gravity')
+    ew_parser.add_argument('--FixFeH',             help='Fix [Fe/H]', action='store_true', metavar='Fix metallicity')
+    ew_parser.add_argument('--Fixmicroturbulence', help='Fix vt',     action='store_true', metavar='Fix microturbulence')
+    ew_parser.add_argument('--tmcalc',             help='Better guess on initial conditions',     action='store_true', metavar='Set initial conditions')
+    ew_parser.add_argument('--refine',             help='Refine parameters',   action='store_true', metavar='Refine parameters')
+    ew_parser.add_argument('--Iterations',         help='Maximum number of iterations', default=160, type=int)
+    ew_parser.add_argument('--outlier',            help='Remove outliers', default='False', choices=['False', '1Iter', '1Once', 'allIter', 'allOnce'])
+    ew_parser.add_argument('--weights',            help='Calculate the slopes of EP and RW with weights', type=str, default='null', choices=['null', 'sigma', 'mad'])
+    ew_parser.add_argument('--EPslope',            help='EP slope to converge', default=0.001, type=float, metavar='EP slope')
+    ew_parser.add_argument('--RWslope',            help='RW slope to converge', default=0.003, type=float, metavar='RW slope')
+    ew_parser.add_argument('--Fedifference',       help='Difference between FeI and FeII', default='0.000', type=float, metavar='|Fel-Fell|')
+    ew_parser.add_argument('--overwrite',          help='Overwrite results.csv', action='store_true', default=False)
+    ew_parser.add_argument('--teffrange',          help='Give warning at high Teff, and remove lines at low Teff', action='store_true', default=False, metavar='Teff range')
+    ew_parser.add_argument('--autofixvt',          help='Auto fix vt if it goes too low or too high', action='store_true', default=False)
+    ew_parser.add_argument('--sigma',              help='Number of sigma, for sigma clipping outliers (default: 3)', type=float, default=3)
+    ew_parser.set_defaults(driver=ew)
 
-    abund_parser = subparsers.add_parser('Abundances', parents=[parent_parser], help='Spectal synthesis for chemical abundance')
-    abund_parser.add_argument('linelist',             help='List with atomic data', widget='FileChooser', metavar='Line List')
-    abund_parser.add_argument('--inter_file',         help='List of wavelength intervals', default='intervals_elements.lst', widget='FileChooser', metavar='Intervals File')
-    abund_parser.add_argument('--element',            help='for abundance determination', choices=['Na', 'Mg', 'Al', 'Si', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Ni'], default='Na', metavar='Element')
-    abund_parser.add_argument('--observations',       help='Input formats: fits, dat, spec', widget='FileChooser', metavar='Observed Spectrum')
-    abund_parser.add_argument('--damping',            help='Parameter for MOOG', default='1', choices=['0', '1', '2'], type=str, metavar='Damping')
-    abund_parser.add_argument('--step_wave',          help='for synthetic spectrum', default=0.01, type=float, metavar='Step Wavelength')
-    abund_parser.add_argument('--step_flux',          help='for synthetic spectrum', default=3.0, type=float, metavar='Step Flux')
-    abund_parser.add_argument('--Iterations',         help='Maximum number', default=100, type=int)
-    abund_parser.add_argument('--resolution',         help='of the spectrograph', default=None, type=int, metavar='Resolution')
-    abund_parser.add_argument('--snr',                help='ratio of spectrum', default=None, type=float, metavar='Signal-to-noise')
-    abund_parser.add_argument('--plot',               help='Plot output', action='store_true', metavar='Plot')
-    abund_parser.add_argument('--plot_res',           help='Add residuals to the plot', action='store_true', metavar='Plot Residuals')
-    abund_parser.add_argument('--limb',               help='coefficient for rotation', default=0.6, type=float, metavar='Limb Darkening')
-    abund_parser.add_argument('--save',               help='Save synthetic spectrum', action='store_true', metavar='Save Output')
+    # For calculating the abundances
+    abund_parser = subparsers.add_parser('abund', parents=[parent_parser], help='Abundances')
+    abund_parser.add_argument('linelist',    help='Input linelist file', widget='FileChooser')
+    abund_parser.add_argument('--overwrite', help='Overwrite abundances.csv', action='store_true', default=False)
     abund_parser.set_defaults(driver=abund)
+
+    # Driver for ARES
+    ares_parser = subparsers.add_parser('ares', help='ARES')
+    ares_parser.add_argument('linelist',    help='Input linelist file', widget='FileChooser')
+    ares_parser.add_argument('spectrum',    help='1D spectrum',         widget='FileChooser')
+    ares_parser.add_argument('--output',    help='Name of the output file (optional)', metavar='Output')
+    ares_parser.add_argument('--lambdai',   help='Start of wavelength interval',  default=3900,  type=int)
+    ares_parser.add_argument('--lambdaf',   help='End of wavelength interval',        default=25000, type=int)
+    ares_parser.add_argument('--smoothder', help='Noise smoother',                    default=4,     type=int)
+    ares_parser.add_argument('--space',     help='Interval for the line computation', default=2.0,   type=float)
+    ares_parser.add_argument('--rejt',      help='Continuum position',                default=False, type=float)
+    ares_parser.add_argument('--lineresol', help='Line resolution',                   default=0.07,  type=float)
+    ares_parser.add_argument('--miniline',  help='Weaker line to be printed out',     default=5.0,   type=float)
+    ares_parser.add_argument('--SNR',       help='If specified, the rejt is calculated', type=float)
+    ares_parser.add_argument('--EWcut',     help='Cut for the maximum EW value',      default=200.0, type=float)
+    ares_parser.add_argument('--RVmask',    help='RV of spectrum (km/s)',             default=False, type=float)
+    ares_parser.add_argument('--force',     help='Remove troublesome lines automatically', default=False, action='store_true', metavar='Force finish')
+    ares_parser.set_defaults(driver=ares)
 
     args = parser.parse_args()
     return args.driver(args)
